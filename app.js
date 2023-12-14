f5cf4b4b8cafa9afedaaea97ea101e4b48d8e9ed
rafael.lumang <lumang.rafael@gmail.com>
2023-12-13 18:14:07 -0800

    fine tuned more styling"

---
+++ app.js
@@ -0,0 +1,12 @@
+
+
+/* ALERT BANNER */
+
+const alertBanner = document.querySelector('#alert');
+
+alertBanner.innerHTML = `<div class="alert-banner">
+    <p> <strong>Alert:</strong> You have <strong> 6</strong> overdue tasks
+to complete</p >
+    <p class="alert-banner-close">x</p>
+</div>`
+
